83e4854c8ca164db1d2060162140fd96fde40030
Philip Kim <filshouse56@gmail.com>
2023-12-12 15:22:44 -0500

    Exercise Completed

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //int answer = 4;
+            var answer = 4;
+
+            //string response;
+            var response = (answer < 9) ? $"{answer} is less than nine" : (answer == 9) ? $"{answer} is equal to nine" : $"{answer} is greater than nine";
+
+            Console.WriteLine(response);
+
+
+            //if (answer < 9)
+            {
+                //response = answer + " is less than nine";
+            }
+            //else
+            {
+               // response = answer + " is greater than or equal to nine";
+            }
         }
     }
 }
